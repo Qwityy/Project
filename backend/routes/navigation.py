b5cafa50da6cd70ed38eae6d0c5a48436e6f1995
Kovtun Ivan <ivan.opera62@gmail.com>
2025-12-08 17:48:10 +0800

    fixed navigation

--- backend/routes/navigation.py
+++ backend/routes/navigation.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
-import json 
+from backend.data.navigation import NAVIGATION
 
 
 router = APIRouter(prefix="/navigation")
 
 
 @router.get("")
-def read_navigation():
-    with open("/Users/mkntsh/Downloads/Project/backend/data/categories.json", "r") as file:
-        navigation = json.load(file)
-        return navigation
+async def read_navigation():
+    return NAVIGATION
+
+    
